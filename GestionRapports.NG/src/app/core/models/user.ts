85a0ba17cca7e17c606aca18fafc844e671fe612
Arman050 <arman.avetissian@edu.technobel.be>
2024-11-26 01:05:53 +0100

    Feature/home front (#37)
    * creation des composant checkReport et addReport, creation de leurs routes
    
    * ajout du composant home, de ses 2 boutons de redirection dans son html, de la fonction pour rediriger sur les routes voulu (addreport et check report)
    
    * ajout du service checkReport et de sa requette http getAll, creation du composant checkreport avec sa fonction pour recuperer la liste d objet qui vient du service et realisation de l html ou on affiche la liste
    
    * retirer le surplus qui aurait du etre dans la feature suivante
    
    * rien
    
    * ajout du style css et du logo html
    
    * update user model
    
    * update user modell
    
    * update user.ts
    
    * gitignore
    
    * idea gitignore add
    
    * update gitignore
    
    ---------
    
    Co-authored-by: Julien <j.colaux5529@gmail.com>

--- GestionRapports.NG/src/app/core/models/user.ts
+++ GestionRapports.NG/src/app/core/models/user.ts
@@ -1,10 +1,18 @@
 export interface User {
+<<<<<<< HEAD
+    User_Id: number;
+    Firstname: string;
+    Lastname: string;
+    Email: string;
+    Profil: number;
+=======
     User_Id?: number;
     Firstname?: string;
     Lastname?: string;
     email: string;
     profil?: string;
     isAdmin?: boolean;
+>>>>>>> develop
     Password: string;
     Phone: string;
 }
